73e91ce0f5327f5680fb9991a6acc0fb59689231
Levon Yeghiazaryan <levon.yeghiazaryan@sentium-consulting.com>
2023-05-23 21:28:06 +0400

    testasd

--- help_gamblers/settings.py
+++ help_gamblers/settings.py
@@ -26,7 +26,7 @@ def get_env_var(var_name, default=None):
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
